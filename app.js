719355b6a0c1397f9541f013934d9bc52e1e7cab
jonathan <jona_egebak@hotmail.dk>
2015-04-07 09:17:36 +0200

    test commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var partials = require('./routes/partials');
 
-
+// hej
 var app = express();
 
 // view engine setup
