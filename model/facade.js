a730a4be5ad3a3d54b65b24ce8c58b72b5c1d09f
Nikolai94 <nikolai@cybermanager.dk>
2015-04-07 12:15:29 +0200

    opg2 new

--- model/facade.js
+++ model/facade.js
@@ -1,10 +1,34 @@
 require("./db");
 
-var User = require("mongoose").model("user");
-var Quotes = require("mongoose").model("quotes");
-var RemoteServers = require("mongoose").model("remoteServers");
+var mong = require("mongoose");
+
+var User = mong.model("user");
+var Quotes = mong.model("quotes");
+var RemoteServers = mong.model("remoteserver");
 
 
-var date = {
+var data = [
+    {firstName: "TestNavn", lastName: "testLastName",userName :"testUser",phone : 12345678,passWord:"1234Pass"},
+    {firstName: "aabb", lastName: "bbaa",userName :"aabb90",phone : 87654321,passWord:"aabbPass"}
+];
 
-};
+//user
+function createUser(data){
+    User.create(data,function(err,user){
+        if(err){
+        return console.log(err);
+        }
+
+        return console.log(user);
+
+    });
+}
+
+function findAllUser(){
+    User.find(function(err,result){
+        if(err)
+            return console.log(err);
+
+        console.log(result);
+    });
+}
